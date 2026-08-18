bad3f83b54006ca20202e154760c255b18147e17
Daniel Moreno <123407190+danmoree@users.noreply.github.com>
2026-08-17 22:30:05 -0600

    Initial Commit

---
+++ pixelScreen/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  pixelScreen
+//
+//  Created by Daniel Moreno on 8/17/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ pixelScreen/pixelScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  pixelScreenApp.swift
+//  pixelScreen
+//
+//  Created by Daniel Moreno on 8/17/26.
+//
+
+import SwiftUI
+
+@main
+struct pixelScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
